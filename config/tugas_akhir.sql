18c8aca095bd0bf12acbf4366d07f4a585de2dc0
rodo <45455107+SianturiRodoLandro@users.noreply.github.com>
2020-01-16 19:09:42 +0700

    Update tugas_akhir.sql

--- config/tugas_akhir.sql
+++ config/tugas_akhir.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2020 at 01:05 PM
+-- Generation Time: Jan 16, 2020 at 01:08 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -21,6 +21,128 @@ SET time_zone = "+00:00";
 --
 -- Database: `tugas_akhir`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity_log`
+--
+
+CREATE TABLE `activity_log` (
+  `log_id` int(11) NOT NULL,
+  `id_user` char(10) NOT NULL,
+  `activity` varchar(250) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `approval`
+--
+
+CREATE TABLE `approval` (
+  `id_formulir` int(11) NOT NULL,
+  `verif_by` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `approval` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departemen`
+--
+
+CREATE TABLE `departemen` (
+  `id_dept` int(11) NOT NULL,
+  `nama_dept` varchar(50) NOT NULL,
+  `cost_center` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `departemen`
+--
+
+INSERT INTO `departemen` (`id_dept`, `nama_dept`, `cost_center`) VALUES
+(1, 'HRD', '81264'),
+(2, 'LnD', '81265');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formulir`
+--
+
+CREATE TABLE `formulir` (
+  `id_pic` int(11) NOT NULL,
+  `id_formulir` int(11) NOT NULL,
+  `id_departemen` int(11) NOT NULL,
+  `requester` varchar(50) NOT NULL,
+  `job_type` varchar(50) NOT NULL,
+  `status_verif` varchar(50) NOT NULL,
+  `status_approved` varchar(50) NOT NULL,
+  `approved_by` varchar(50) NOT NULL,
+  `verif_by` varchar(50) NOT NULL,
+  `open_position` varchar(50) NOT NULL,
+  `budget` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `id_dept` int(11) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `marital_status` varchar(50) NOT NULL,
+  `tanggal_masuk` date DEFAULT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL,
+  `status_karyawan` varchar(50) NOT NULL,
+  `tempat_lahir` varchar(50) NOT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `alamat` text NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `no_telepon` varchar(20) NOT NULL,
+  `hak_akses` enum('admin','pic','manager') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`id_karyawan`, `id_dept`, `username`, `password`, `nama`, `jabatan`, `marital_status`, `tanggal_masuk`, `jenis_kelamin`, `status_karyawan`, `tempat_lahir`, `tanggal_lahir`, `alamat`, `email`, `no_telepon`, `hak_akses`) VALUES
+(1, 1, 'admin', '123', 'Rodo Landro Sianturi', 'HR', 'M1', '2018-03-13', 'Laki-laki', 'Permanen', 'Palembang', '2019-09-08', 'adcadcvad', 'rodo_landro.sianturi@alcon365.com', '085272216320', 'admin'),
+(2, 1, 'admin', '123', 'Rodo Sianturi', 'HR Officer', 'M1', '2014-12-15', 'Laki-Laki', 'Permanen', 'Palembang', '1978-10-16', 'Perum. Pondok Idaman - Batam Center', 'rodo_landro.sianturi@alcon365.com', '085272216320', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manager`
+--
+
+CREATE TABLE `manager` (
+  `id_dept` int(11) NOT NULL,
+  `id_karyawan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesan`
+--
+
+CREATE TABLE `pesan` (
+  `id_pesan` int(11) NOT NULL,
+  `pengirim` int(11) NOT NULL,
+  `detail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -51,6 +173,50 @@ INSERT INTO `users` (`user_id`, `nama`, `username`, `password`, `departemen`, `h
 --
 
 --
+-- Indexes for table `activity_log`
+--
+ALTER TABLE `activity_log`
+  ADD PRIMARY KEY (`log_id`);
+
+--
+-- Indexes for table `approval`
+--
+ALTER TABLE `approval`
+  ADD PRIMARY KEY (`id_formulir`,`verif_by`),
+  ADD KEY `verif_by` (`verif_by`);
+
+--
+-- Indexes for table `departemen`
+--
+ALTER TABLE `departemen`
+  ADD PRIMARY KEY (`id_dept`);
+
+--
+-- Indexes for table `formulir`
+--
+ALTER TABLE `formulir`
+  ADD PRIMARY KEY (`id_formulir`);
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indexes for table `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`id_dept`),
+  ADD UNIQUE KEY `id_karyawan` (`id_karyawan`);
+
+--
+-- Indexes for table `pesan`
+--
+ALTER TABLE `pesan`
+  ADD KEY `pengirim` (`pengirim`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -61,10 +227,52 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `departemen`
+--
+ALTER TABLE `departemen`
+  MODIFY `id_dept` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `formulir`
+--
+ALTER TABLE `formulir`
+  MODIFY `id_formulir` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `approval`
+--
+ALTER TABLE `approval`
+  ADD CONSTRAINT `approval_ibfk_1` FOREIGN KEY (`id_formulir`) REFERENCES `formulir` (`id_formulir`),
+  ADD CONSTRAINT `approval_ibfk_2` FOREIGN KEY (`verif_by`) REFERENCES `karyawan` (`id_karyawan`);
+
+--
+-- Constraints for table `manager`
+--
+ALTER TABLE `manager`
+  ADD CONSTRAINT `manager_ibfk_1` FOREIGN KEY (`id_dept`) REFERENCES `departemen` (`id_dept`) ON DELETE CASCADE,
+  ADD CONSTRAINT `manager_ibfk_2` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `pesan`
+--
+ALTER TABLE `pesan`
+  ADD CONSTRAINT `pesan_ibfk_1` FOREIGN KEY (`pengirim`) REFERENCES `karyawan` (`id_karyawan`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
