8ebe0c7a54d739d71364de68a33c745dcb3ecb65
rodo <45455107+SianturiRodoLandro@users.noreply.github.com>
2020-01-28 21:48:52 +0700

    update index.php (form login)

--- index.php
+++ index.php
@@ -27,79 +27,79 @@ session_start();
   	<!-- Favicon-->
   	<link rel="icon" href="img/alcr.jpg" type="image/x-icon">
 
-<style>
-	body, html {
-	  height: 100%;
-	  font-family: Arial, Helvetica, sans-serif;
-	}
+	<style>
+		body, html {
+		height: 100%;
+		font-family: Arial, Helvetica, sans-serif;
+		}
 
-	* {
-	  box-sizing: border-box;
-	}
+		* {
+		box-sizing: border-box;
+		}
 
-	.bg-img {
-	  /* The image used */
-	  background-image: url("img/CVB.jpg");
+		.bg-img {
+		/* The image used */
+		background-image: url("img/CVB.jpg");
 
-	  min-height: 70%;
+		min-height: 70%;
 
-	  /* Center and scale the image nicely */
-	  background-position: center;
-	  background-repeat: no-repeat;
-	  background-size: cover;
-	  position: relative;
-	}
+		/* Center and scale the image nicely */
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		position: relative;
+		}
 
-	/* Add styles to the form container */
-	.container {
-	  position: absolute;
-	  right: 0;
-	  margin: 20px;
-	  max-width: 300px;
-	  padding: 16px;
-	  background-color: white;
-	}
+		/* Add styles to the form container */
+		.container {
+		position: absolute;
+		right: 0;
+		margin: 20px;
+		max-width: 300px;
+		padding: 16px;
+		background-color: white;
+		}
 
-	/* Full-width input fields */
-	input[type=text], input[type=password] {
-	  width: 100%;
-	  padding: 15px;
-	  margin: 5px 0 22px 0;
-	  border: none;
-	  background: #f1f1f1;
-	}
+		/* Full-width input fields */
+		input[type=text], input[type=password] {
+		width: 100%;
+		padding: 15px;
+		margin: 5px 0 22px 0;
+		border: none;
+		background: #f1f1f1;
+		}
 
-	input[type=text]:focus, input[type=password]:focus {
-	  background-color: #ddd;
-	  outline: none;
-	}
+		input[type=text]:focus, input[type=password]:focus {
+		background-color: #ddd;
+		outline: none;
+		}
 
-	/* Set a style for the submit button */
-	.btn {
-	  background-color: #4CAF50;
-	  color: white;
-	  padding: 16px 20px;
-	  border: none;
-	  cursor: pointer;
-	  width: 100%;
-	  opacity: 0.9;
-	}
+		/* Set a style for the submit button */
+		.btn {
+		background-color: #40a8f7;
+		color: white;
+		padding: 16px 20px;
+		border: none;
+		cursor: pointer;
+		width: 100%;
+		opacity: 0.9;
+		}
 
-	.btn:hover {
-	  opacity: 1;
-	}
+		.btn:hover {
+		opacity: 1;
+		}
 
-	.footer {
-   position: fixed;
-   left: 0;
-   bottom: 0;
-   width: 100%;
-   background-color: blue;
-   color: white;
-   text-align: center;
-   }
+		.footer {
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		background-color: #4080f7;
+		color: white;
+		text-align: center;
+		}
 
-</style>
+	</style>
 
 </head>
 
