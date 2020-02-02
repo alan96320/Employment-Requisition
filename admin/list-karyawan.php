6c03a40f43f6898765acae5fb27596c07a1643a3
rodo <45455107+SianturiRodoLandro@users.noreply.github.com>
2020-02-02 18:06:47 +0700

    Update list-karyawan.php

--- admin/list-karyawan.php
+++ admin/list-karyawan.php
@@ -5,9 +5,10 @@ if (!isset($_SESSION['username'])) {
   header("location: index.php");
 }
 
-$stm = $pdo_conn->prepare("SELECT `id_karyawan`, `username`, karyawan.`nama`, `jabatan`, `tanggal_masuk`, `jenis_kelamin`, `status_karyawan`, `tempat_lahir`, `tanggal_lahir`, `alamat`, `email`, `no_telepon`, departemen.nama_dept , marital_status.nama as nama_marital_status
+$stm = $pdo_conn->prepare("SELECT `id_karyawan`, `username`, karyawan.`nama`, jabatan.nama as `jabatan`, `tanggal_masuk`, `jenis_kelamin`, `status_karyawan`, `tempat_lahir`, `tanggal_lahir`, `alamat`, `email`, `no_telepon`, departemen.nama_dept , marital_status.nama as nama_marital_status
                            FROM `karyawan` 
                            LEFT JOIN departemen ON karyawan.id_dept = departemen.id_dept
+                           LEFT JOIN jabatan ON karyawan.jabatan = jabatan.id_jabatan
 						               LEFT JOIN marital_status ON karyawan.marital_status = marital_status.id_marital");
 
 $stm->execute();
