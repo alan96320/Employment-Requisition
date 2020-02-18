00c3459b034f5db5bfb74609b7a06a0294156842
rodo <45455107+SianturiRodoLandro@users.noreply.github.com>
2020-02-19 00:10:17 +0700

    Update tambah-karyawan.php
    admin\tambah-karyawan.php

--- admin/tambah-karyawan.php
+++ admin/tambah-karyawan.php
@@ -192,7 +192,7 @@ $rowsJabatan = $stmJab->fetchAll(PDO::FETCH_ASSOC);
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel"> Admin ready Log Out?</h5>
+          <h5 class="modal-title" id="exampleModalLabel"> Admin ready to Log Out?</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
